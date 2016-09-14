2d6b7452a420fa9204c9b04cd900c58b6875cb93
Rivu Chakraborty <rivu.chakraborty@massoftind.com>
2016-09-14 17:38:38 +0530

    Photo Selection Complete

--- app/src/main/java/com/massoftind/rnd/firechatonetoone/RegisterActivity.java
+++ app/src/main/java/com/massoftind/rnd/firechatonetoone/RegisterActivity.java
@@ -1,7 +1,16 @@
 package com.massoftind.rnd.firechatonetoone;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,7 +22,13 @@ import android.widget.Toast;
 import com.massoftind.rnd.firechatonetoone.adapters.LoginRegisterAdapter;
 import com.massoftind.rnd.firechatonetoone.datamodal.LoginRegisterDatamodel;
 import com.massoftind.rnd.firechatonetoone.interfaces.OnRecyclerViewCellClick;
+import com.massoftind.rnd.firechatonetoone.utils.Utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -21,6 +36,9 @@ public class RegisterActivity extends AppCompatActivity {
     private RecyclerView loginRecyclerView;
     private LoginRegisterAdapter adapter;
     private ArrayList<LoginRegisterDatamodel> registerItems;
+
+    private static final int REQUEST_CAMERA = 101;
+    private static final int SELECT_FILE = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +76,8 @@ public class RegisterActivity extends AppCompatActivity {
                         @Override
                         public void onClick(View view) {
                             //Upload Profile Image
-                            Toast.makeText(getBaseContext(),"Yet to Implement",Toast.LENGTH_LONG).show();
+//                            Toast.makeText(getBaseContext(),"Yet to Implement",Toast.LENGTH_LONG).show();
+                            selectImage();
                         }
                     },null);
                     break;
@@ -100,5 +119,100 @@ public class RegisterActivity extends AppCompatActivity {
                 registerItems.add(loginRegisterDatamodel);
             }
         }
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) { // get absolute file path from the galary image uri
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+                File destination = new File(Environment.getExternalStorageDirectory(),
+                        "UsersApp_" + System.currentTimeMillis() + ".jpg");
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String fileAddress = destination.getAbsolutePath();
+                registerItems.get(0).setBitmapProfile(thumbnail);
+                adapter.notifyItemChanged(0);
+            } else if (requestCode == SELECT_FILE) {
+                Bitmap bm = null;
+                if (data != null) {
+                    try {
+                        bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                        String fileAddress = getRealPathFromURI(this, data.getData());
+                        if(null != bm){
+                            registerItems.get(0).setBitmapProfile(bm);
+                            adapter.notifyItemChanged(0);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+//                attachImg.setImageBitmap(bm);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        selectImage();
+    }
+
+    public void selectImage(){
+        Utils.hideKeyboard(this);
+        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utils.checkPermission(RegisterActivity.this);
+                if (items[item].equals("Take Photo")) {
+//                    userChoosenTask = "Take Photo";
+                    if (result) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, REQUEST_CAMERA);
+                    }
+                } else if (items[item].equals("Choose from Library")) {
+//                    userChoosenTask = "Choose from Library";
+                    if (result) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+                    }
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 }
--- app/src/main/java/com/massoftind/rnd/firechatonetoone/utils/Utils.java
+++ app/src/main/java/com/massoftind/rnd/firechatonetoone/utils/Utils.java
@@ -1,10 +1,18 @@
 package com.massoftind.rnd.firechatonetoone.utils;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
@@ -20,6 +28,7 @@ import android.widget.EditText;
 public class Utils {
 
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 111;
 
     public static void hideKeyboard(Activity activity) {
         try {
@@ -190,5 +199,38 @@ public class Utils {
                 + ", reqWidth " + reqWidth + ", reqHeight " + reqHeight + ", inSampleSize " + inSampleSize);
 
         return inSampleSize;
+    }
+
+    public static boolean checkPermission(final Context context)
+    {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if(currentAPIVersion>=android.os.Build.VERSION_CODES.M)
+        {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)||ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.CAMERA)) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("External storage and Camera permission is necessary");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                        }
+                    });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
     }
 }
